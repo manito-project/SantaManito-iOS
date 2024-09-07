514ea5e9f5ec944f2648af1fc3035acf221925a2
Melt <seokwoo2000@naver.com>
2024-09-07 14:15:54 +0900

    [Chore] #6 - 색상변경

--- SantaManito-iOS/SantaManito-iOS/DSKit/Style/ButtonStyles.swift
+++ SantaManito-iOS/SantaManito-iOS/DSKit/Style/ButtonStyles.swift
@@ -13,3 +13,20 @@ struct ScaleButtonStyle: ButtonStyle {
             .scaleEffect(configuration.isPressed ? 0.95 : 1)
     }
 }
+
+struct SMBottomButtonStyle: ButtonStyle {
+    
+    @Environment(\.isEnabled) private var isEnabled: Bool
+
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .frame(height: 52)
+            .frame(maxWidth: .infinity)
+            .font(.system(size: 20, weight: .semibold)) //TODO: 폰트 변경
+            .background(isEnabled ? .smRed : .smLightgray)
+            .foregroundStyle(.smWhite)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .scaleEffect(configuration.isPressed ? 0.95 : 1)
+            .padding(.horizontal, 16)
+    }
+}
