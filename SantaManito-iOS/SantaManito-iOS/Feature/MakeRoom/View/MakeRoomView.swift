3effa8fc7aedcb8c806eb179601efda6a786d100
HELLOHIDI <fbgmlwo123@naver.com>
2024-09-09 10:49:33 +0900

    [Comment] 주석을 통해 비즈니스 로직 정리 #5

---
+++ SantaManito-iOS/SantaManito-iOS/Feature/MakeRoom/View/MakeRoomView.swift
@@ -0,0 +1,18 @@
+//
+//  MakeRoomView.swift
+//  SantaManito-iOS
+//
+//  Created by 류희재 on 9/9/24.
+//
+
+import SwiftUI
+
+struct MakeRoomView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MakeRoomView()
+}
