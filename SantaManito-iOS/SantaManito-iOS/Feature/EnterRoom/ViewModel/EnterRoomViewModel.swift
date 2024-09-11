15e0d099fc0b7d25c6a2b8f3cc8c2d442d3f9dc8
HELLOHIDI <fbgmlwo123@naver.com>
2024-09-11 18:37:22 +0900

    [Feat] 방 입장하기 뷰 비즈니스 로직 구현 #13

--- SantaManito-iOS/SantaManito-iOS/Feature/EnterRoom/ViewModel/EnterRoomViewModel.swift
+++ SantaManito-iOS/SantaManito-iOS/Feature/EnterRoom/ViewModel/EnterRoomViewModel.swift
@@ -13,21 +13,48 @@ class EnterRoomViewModel: ObservableObject {
     //MARK: Action
     
     enum Action {
-        
+        case editInviteCode
+        case enterButtonDidClicked
     }
     
+    @Published var inviteCode: String = ""
+    
     @Published private(set) var state = State(
-        
+        isEnabled: false,
+        canPush: false,
+        isValid: false,
+        errMessage: "이미 참여중인 방입니다!"
     )
     
     //MARK: State
     
     struct State {
-        
+        var isEnabled: Bool
+        var canPush: Bool // 다음 화면으로 넘어갈 수 있는지를 나타내는 상태값
+        var isValid: Bool // 에러가 있는지 없는지 즉, 초대코드가 유효한지 근데 이걸 errMessage로 구분할지 애매한 상황
+        var errMessage: String
     }
     
     //MARK: send
     
     func send(action: Action) {
+        switch action {
+        case .editInviteCode:
+            configButtonisEnabled()
+        case .enterButtonDidClicked:
+            //TODO: 초대코드 유효성 검사
+            configisValid()
+        }
     }
+}
+
+extension EnterRoomViewModel {
+    func configButtonisEnabled() {
+        state.isEnabled = !inviteCode.isEmpty
+    }
+    
+    func configisValid() {
+        state.isValid = !state.errMessage.isEmpty
+    }
+
 }
