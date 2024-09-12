5bbd84a6211f64b660485007bf09cbb67792a0e3
HELLOHIDI <fbgmlwo123@naver.com>
2024-09-12 15:02:42 +0900

    [Feat] 매칭 애니메이션 뷰 UI, 비즈니스 로직 구현 #14

--- SantaManito-iOS/SantaManito-iOS/Feature/MatchingRoom/ViewModel/MatchingResultViewModel.swift
+++ SantaManito-iOS/SantaManito-iOS/Feature/MatchingRoom/ViewModel/MatchingResultViewModel.swift
@@ -13,22 +13,28 @@ class MatchingViewModel: ObservableObject {
     //MARK: Action
     
     enum Action {
-        
+        case load
     }
     
     @Published private(set) var state = State(
-        
+        isAnimating: false
     )
     
     //MARK: State
     
     struct State {
-        
+        var isAnimating: Bool
     }
     
     //MARK: send
     
     func send(action: Action) {
+        switch action {
+        case .load:
+            //TODO: 마니또 서버 통신
+            //TODO: isMatched 변수 변경
+            break
+        }
     }
 }
 
