88e4f8b37fb4ac8599e60cdd560895306695090c
HELLOHIDI <fbgmlwo123@naver.com>
2024-09-16 00:14:19 +0900

    [Feat] 결과공개뷰 UI 구현 #14

---
+++ SantaManito-iOS/SantaManito-iOS/Feature/Finish/ViewModel/FinishViewModel.swift
@@ -0,0 +1,33 @@
+//
+//  FinishViewModel.swift
+//  SantaManito-iOS
+//
+//  Created by 류희재 on 9/12/24.
+//
+
+import Foundation
+import Combine
+
+class FinishViewModel: ObservableObject {
+    
+    //MARK: Action
+    
+    enum Action {
+        
+    }
+    
+    @Published private(set) var state = State(
+        
+    )
+    
+    //MARK: State
+    
+    struct State {
+        
+    }
+    
+    //MARK: send
+    
+    func send(action: Action) {
+    }
+}
