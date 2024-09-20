cedf66e3c42a1f3d307b742b53710cc165d1bfd8
HELLOHIDI <fbgmlwo123@naver.com>
2024-09-20 12:31:50 +0900

    [Feat] 방 정보 설정 뷰 StubService 연결 #25

---
+++ SantaManito-iOS/SantaManito-iOS/Data/Service/Room/EditRoomService.swift
@@ -0,0 +1,32 @@
+//
+//  EditRoomService.swift
+//  SantaManito-iOS
+//
+//  Created by 류희재 on 9/20/24.
+//
+
+import Foundation
+import Combine
+
+
+protocol EditRoomServiceType {
+    func getRoomInfo(with roomID: String) -> AnyPublisher<MakeRoomInfo, Error>
+    func editRoomInfo(with roomID: String, roomInfo: MakeRoomInfo) -> AnyPublisher<Void, Error>
+}
+
+struct StubEditRoomService: EditRoomServiceType {
+    func editRoomInfo(with roomID: String, roomInfo: MakeRoomInfo) -> AnyPublisher<Void, any Error> {
+        return Just(()).setFailureType(to: Error.self).eraseToAnyPublisher()
+    }
+    
+    func getRoomInfo(with roomID: String) -> AnyPublisher<MakeRoomInfo, Error> {
+        return Just(
+            MakeRoomInfo(
+                name: "마니또 방 이름",
+                remainingDays: 10,
+                dueDate: Date()
+            )
+        ).setFailureType(to: Error.self).eraseToAnyPublisher()
+    }
+}
+
