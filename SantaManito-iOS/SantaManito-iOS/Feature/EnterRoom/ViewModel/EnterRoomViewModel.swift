fea54bbe2d49deaee4ddbf5b23da68fdc9b91035
HELLOHIDI <fbgmlwo123@naver.com>
2024-09-21 14:57:41 +0900

    [Feat] EnterRoomViewModel 비즈니스 로직 구체화 #26

--- SantaManito-iOS/SantaManito-iOS/Feature/EnterRoom/ViewModel/EnterRoomViewModel.swift
+++ SantaManito-iOS/SantaManito-iOS/Feature/EnterRoom/ViewModel/EnterRoomViewModel.swift
@@ -17,12 +17,11 @@ class EnterRoomViewModel: ObservableObject {
         case editInviteCode
         case enterButtonDidClicked
     }
-    
     struct State {
-        var isEnabled: Bool = false
-        var canPush: Bool = false // 다음 화면으로 넘어갈 수 있는지를 나타내는 상태값
+        var isEnabled: Bool = false //MARK: 히디의 고민: isEnabled라는 변수명이 너무 모호함 (초대코드에 텍스트가 있으면 버튼을 누를 수 있도록 하는 역할)
+        var canPush: Bool = false // 다음 화면으로 넘어갈 수 있는지를 나타내는 상태값 //MARK: 히디의 고민: canPush 조금 이상한? 느낌 ㅠ
         var isValid: Bool = true // 에러가 있는지 없는지 즉, 초대코드가 유효한지 근데 이걸 errMessage로 구분할지 애매한 상황
-        var errMessage: String?
+        var errMessage: String = ""
     }
     
     //MARK: Dependency
@@ -51,9 +50,12 @@ class EnterRoomViewModel: ObservableObject {
         case .editInviteCode:
             configButtonisEnabled()
         case .enterButtonDidClicked:
+            print("버튼 눌림")
             roomService.validateParticipationCode(inviteCode: inviteCode)
                 .sink(receiveCompletion: { completion in
+                    print("처리됨")
                     if case .failure(let error) = completion {
+                        owner.state.isValid = false
                         switch error {
                         case .deletedRoomCode, .invalidateCode, .alreadyMatchedError:
                             owner.state.errMessage = "참여가 불가능한 방이야! 혹시 초대코드에 공백이 있는지 확인해줘."
@@ -63,6 +65,7 @@ class EnterRoomViewModel: ObservableObject {
                     }
                 }, receiveValue: { _ in
                     owner.state.isValid = true
+                    owner.state.canPush = true
                 })
                 .store(in: cancelBag)
         }
