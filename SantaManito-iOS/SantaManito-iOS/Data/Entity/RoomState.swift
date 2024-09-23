3ae2555403fd1303414a474bc52ac3eaa4f3b652
Melt <seokwoo2000@naver.com>
2024-09-24 00:00:42 +0900

    [Feat] #30 - Room Detail Entity, Response 선작업

---
+++ SantaManito-iOS/SantaManito-iOS/Data/Entity/RoomState.swift
@@ -0,0 +1,15 @@
+//
+//  RoomState.swift
+//  SantaManito-iOS
+//
+//  Created by 장석우 on 9/23/24.
+//
+
+import Foundation
+
+enum RoomState: Hashable {
+    case notStarted
+    case inProgress
+    case completed
+    case deleted
+}
--- SantaManito-iOS/SantaManito-iOS/Feature/Home/HomeViewModel.swift
+++ SantaManito-iOS/SantaManito-iOS/Feature/Home/HomeViewModel.swift
@@ -9,7 +9,7 @@ import Combine
 
 class HomeViewModel: ObservableObject {
     
-//MARK: - Action, State
+    //MARK: - Action, State
     
     enum Action {
         case onAppear
@@ -22,7 +22,7 @@ class HomeViewModel: ObservableObject {
     
     
     struct State {
-        var rooms: [RoomInfo] = []
+        var rooms: [RoomDetail] = []
     }
     
     //MARK: - Dependency
@@ -30,11 +30,12 @@ class HomeViewModel: ObservableObject {
     var roomService: RoomServiceType
     private var navigationRouter: NavigationRoutableType
     
+    
     //MARK: - Properties
     
     @Published var state = State()
     private let cancelBag = CancelBag()
-
+    
     //MARK: - Init
     
     init(
@@ -56,13 +57,24 @@ class HomeViewModel: ObservableObject {
             
         case .onAppear:
             roomService.fetch()
+                .flatMap { roomInfos -> AnyPublisher<[RoomDetail], Error> in
+                    // Step 2: 각 RoomInfo에서 roomID를 가져와 fetch(with:) 호출
+                    let roomDetailsPublishers = roomInfos.map { roomInfo in
+                        self.roomService.fetch(with: roomInfo.id) // fetch(with:) 결과를 가져옴
+                    }
+                    
+                    // Step 3: 결과들을 배열로 묶어서 반환 (Publishers.MergeMany 사용)
+                    return Publishers.MergeMany(roomDetailsPublishers) // 여러 Publisher를 하나로 합침
+                        .collect() // RoomDetail을 하나의 배열로 묶음
+                        .eraseToAnyPublisher() // AnyPublisher로 반환
+                }
                 .catch { _ in Empty() }
-                .assign(to: \.state.rooms, on: owner)
+                .assign(to: \.state.rooms, on: self)
                 .store(in: cancelBag)
             
         case .myPageButtonDidTap:
             navigationRouter.push(to: .myPage)
-        
+            
         case .makeRoomButtonDidTap:
             navigationRouter.push(to: .editRoom(viewType: .createMode))
             
@@ -77,4 +89,4 @@ class HomeViewModel: ObservableObject {
         }
     }
 }
-    
+
