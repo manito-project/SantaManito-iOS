ee185069a0cfd0cac9471232fb838e37a2f2e30a
HELLOHIDI <fbgmlwo123@naver.com>
2024-09-24 00:46:34 +0900

    [Feat] ShowAllResultView 비즈니스 로직 구체화 #30

--- SantaManito-iOS/SantaManito-iOS/Data/Service/Room/MatchRoomService.swift
+++ SantaManito-iOS/SantaManito-iOS/Data/Service/Room/MatchRoomService.swift
@@ -8,20 +8,84 @@
 import Foundation
 import Combine
 
-struct ManitoUser {
-    var manito: String
-    var mission: String
-}
+struct MatchingFinishData : Codable, Hashable {
+    let userID, santaUserID, manittoUserID: Int
+    let myMission, missionToMe: MissionToMe
+    let santaUsername, manittoUsername: String
 
-extension ManitoUser {
-    static var stub1: ManitoUser {
-        .init(manito: "류희재", mission: "다고다고다고다고다고다고다고다고다고다고다고다고다고다고다고다고다고다고다고다고다고다고다고다고다고다고ㅍ")
+    enum CodingKeys: String, CodingKey {
+        case userID = "UserId"
+        case santaUserID = "SantaUserId"
+        case manittoUserID = "ManittoUserId"
+        case myMission = "MyMission"
+        case missionToMe = "MissionToMe"
+        case santaUsername = "SantaUsername"
+        case manittoUsername = "ManittoUsername"
     }
 }
 
+// MARK: - MissionToMe
+struct MissionToMe: Codable, Hashable {
+    let content: String
+}
+
+extension MatchingFinishData {
+    static var stub1: MatchingFinishData {
+        .init(
+            userID: 1,
+            santaUserID: 2,
+            manittoUserID: 1,
+            myMission: MissionToMe(content: "뭐시기뭐시기뭐시기뭐시기"),
+            missionToMe: MissionToMe(content: "뭐시기뭐시기뭐시기뭐시기"),
+            santaUsername: "류희재",
+            manittoUsername: "장석우"
+        )
+    }
+    
+    static var stub2: MatchingFinishData {
+        .init(
+            userID: 1,
+            santaUserID: 2,
+            manittoUserID: 1,
+            myMission: MissionToMe(content: "뭐시기뭐시기뭐시기뭐시기"),
+            missionToMe: MissionToMe(content: "뭐시기뭐시기뭐시기뭐시기"),
+            santaUsername: "류희재",
+            manittoUsername: "장석우"
+        )
+    }
+    
+    static var stub3: MatchingFinishData {
+        .init(
+            userID: 1,
+            santaUserID: 2,
+            manittoUserID: 1,
+            myMission: MissionToMe(content: "뭐시기뭐시기뭐시기뭐시기"),
+            missionToMe: MissionToMe(content: "뭐시기뭐시기뭐시기뭐시기"),
+            santaUsername: "류희재",
+            manittoUsername: "장석우"
+        )
+    }
+    
+    static var stub4: MatchingFinishData {
+        .init(
+            userID: 1,
+            santaUserID: 2,
+            manittoUserID: 1,
+            myMission: MissionToMe(content: "뭐시기뭐시기뭐시기뭐시기"),
+            missionToMe: MissionToMe(content: "뭐시기뭐시기뭐시기뭐시기"),
+            santaUsername: "류희재",
+            manittoUsername: "장석우"
+        )
+    }
+}
+
+
+
 protocol MatchRoomServiceType {
     func matchPlayer() -> AnyPublisher<Void, Error>
-    func getManito(_ userID: String) -> AnyPublisher<ManitoUser, Error>
+    func getManito(_ userID: String) -> AnyPublisher<MatchingFinishData, Error>
+    func getManitoResult(_ roomID: String) -> AnyPublisher<[MatchingFinishData], Error>
+    func deleteRoom(_ roomID: String) -> AnyPublisher<Void, Error>
 }
 
 struct StubMatchRoomService: MatchRoomServiceType {
@@ -30,9 +94,18 @@ struct StubMatchRoomService: MatchRoomServiceType {
         return Just(()).setFailureType(to: Error.self).eraseToAnyPublisher()
     }
     
-    func getManito(_ userID: String) -> AnyPublisher<ManitoUser, Error> {
+    func getManito(_ userID: String) -> AnyPublisher<MatchingFinishData, Error> {
         return Just(.stub1).setFailureType(to: Error.self).eraseToAnyPublisher()
     }
+    
+    func getManitoResult(_ roomID: String) -> AnyPublisher<[MatchingFinishData], Error> {
+        return Just([.stub1, .stub2, .stub3, .stub4]).setFailureType(to: Error.self).eraseToAnyPublisher()
+    }
+    
+    func deleteRoom(_ roomID: String) -> AnyPublisher<Void, Error> {
+        return Just(()).setFailureType(to: Error.self).eraseToAnyPublisher()
+    }
 }
+
 
 
