51d67b2c54ea6ddefd61a6782e22e59d3f4bd08b
Melt <seokwoo2000@naver.com>
2024-10-01 22:59:12 +0900

    [Feat] #47 - Firebase AppDelegate 연동

---
+++ SantaManito-iOS/SantaManito-iOS/App/AppDelegate.swift
@@ -0,0 +1,47 @@
+//
+//  AppDelegate.swift
+//  SantaManito-iOS
+//
+//  Created by 장석우 on 10/1/24.
+//
+
+import UIKit
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    
+    FirebaseApp.configure()
+    
+    return true
+  }
+  
+
+  
+  
+}
+
+//MARK: - 추후 FCM 구현시
+//extension AppDelegate {
+//
+//    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+//      Messaging.messaging().apnsToken = deviceToken
+//      print(deviceToken)
+//    }
+//}
+//
+//extension AppDelegate: UNUserNotificationCenterDelegate {
+//  
+//  //Foreground 상태에서 푸시 받을 때
+//  func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+//    completionHandler([.banner, .sound])
+//  }
+//  
+//  
+//  func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+//    completionHandler()
+//  }
+//}
+//
+//
