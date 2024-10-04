a49ce8bc7761e322773d7b749b1047053ee3b608
Melt <seokwoo2000@naver.com>
2024-10-04 20:53:07 +0900

    [FEAT] #52 - AppService 버전 가져오는 기능 구현(local, appStore) + AppService 테스트 코드 구현

--- SantaManito-iOS/SantaManito-iOS/Data/Service/AppService.swift
+++ SantaManito-iOS/SantaManito-iOS/Data/Service/AppService.swift
@@ -9,20 +9,40 @@ import Foundation
 
 protocol AppServiceType {
     func getLocalAppVersion() -> Version
+    func getAppStoreVersion() -> Version
 }
 
 struct AppService: AppServiceType {
     
     func getLocalAppVersion() -> Version {
         guard let dictionary = Bundle.main.infoDictionary,
-              let version = dictionary["CFBundleShortVersionString"] as? String 
-        else { return Version("1.0.0") }
+              let version = dictionary["CFBundleShortVersionString"] as? String
+        else { return Version("0.0.0") }
         return Version(version)
     }
-}
     
+    func getAppStoreVersion() -> Version {
+        guard let bundleID = Bundle.main.infoDictionary?["CFBundleIdentifier"] as? String 
+        else { return Version("\(Int.max).0.0") }
+        
+        let appStoreUrl = "http://itunes.apple.com/lookup?bundleId=\(bundleID)"
+        guard let url = URL(string: appStoreUrl),
+              let data = try? Data(contentsOf: url),
+              let json = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any],
+              let results = json["results"] as? [[String: Any]],
+              let appStoreVersion = results[0]["version"] as? String
+        else { return Version("0.0.0") }
+        
+        return Version(appStoreVersion)
+    }
+}
+
 struct StubAppService: AppServiceType {
     func getLocalAppVersion() -> Version {
-        return Version("1.0.0") 
+        return Version("2.0.0")
+    }
+    
+    func getAppStoreVersion() -> Version {
+        return Version("1.2.2")
     }
 }
