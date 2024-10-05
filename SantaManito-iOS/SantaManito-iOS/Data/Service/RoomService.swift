59958a24c21a2fdf23834e55e2227f3a15e958ce
Melt <seokwoo2000@naver.com>
2024-10-05 13:29:39 +0900

    [Chore] #54 - RoomService 수정

--- SantaManito-iOS/SantaManito-iOS/Data/Service/RoomService.swift
+++ SantaManito-iOS/SantaManito-iOS/Data/Service/RoomService.swift
@@ -8,14 +8,38 @@ import Foundation
 import Combine
 
 protocol RoomServiceType {
+    
+    // All
     func fetchAll() -> AnyPublisher<[RoomDetail], Error>
     func fetch(with roomID: String) -> AnyPublisher<RoomDetail, Error>
+    func exit(with roomID: String) -> AnyPublisher<Void, Error>
+    
+    
+    // Creator
+    func create(with info: MakeRoomInfo) -> AnyPublisher<String, Error>
     func edit(with roomID: String) -> AnyPublisher<Void, Error>
     func delete(with roomID: String) -> AnyPublisher<Void, Error>
     
+    
+    // Guest
+    func enter(at invitationCode: String) -> AnyPublisher<Void,Error>
 }
 
 struct StubRoomService: RoomServiceType {
+    func exit(with roomID: String) -> AnyPublisher<Void, Error> {
+        Just(()).setFailureType(to: Error.self).eraseToAnyPublisher()
+    }
+    
+    func enter(at invitationCode: String) -> AnyPublisher<Void, Error> {
+        Just(()).setFailureType(to: Error.self).eraseToAnyPublisher()
+    }
+    
+
+    
+    func create(with info: MakeRoomInfo) -> AnyPublisher<String, Error> {
+        Just("초대코드1").setFailureType(to: Error.self).eraseToAnyPublisher()
+    }
+    
     
     func fetchAll() -> AnyPublisher<[RoomDetail], Error> {
         Future<[RoomDetail],Error> { promise in
