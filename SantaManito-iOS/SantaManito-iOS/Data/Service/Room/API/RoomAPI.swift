3059d4f088bfbfca1ecf9cff974773ae9170f8b9
HELLOHIDI <fbgmlwo123@naver.com>
2024-10-07 12:39:48 +0900

    [Chore] API 합칠 수 있는거 합치기 #55

---
+++ SantaManito-iOS/SantaManito-iOS/Data/Service/Room/API/RoomAPI.swift
@@ -0,0 +1,96 @@
+//
+//  RoomAPI.swift
+//  SantaManito-iOS
+//
+//  Created by 류희재 on 9/30/24.
+//
+
+import Foundation
+
+enum RoomAPI {
+    case createRoom(request: CreateRoomRequest)
+    case getEnteredAllRoom
+    case getRoomInfo(roomID: String)
+    case editRoomInfo(roomID: String, request: EditRoomRequest)
+    case deleteRoom(roomID: String)
+    
+    case enterRoom(request: EnterRoomRequest)
+    case matchRoom(roomID: String)
+    case exitRoom(roomID: String)
+    case deleteHistoryRoom(roomID: String)
+}
+
+extension RoomAPI: BaseAPI {
+    var path: String? {
+        switch self {
+        case .createRoom:
+            return Paths.createRoom
+        case .getEnteredAllRoom:
+            return Paths.getRooms
+        case .getRoomInfo(let roomID):
+            return Paths.getRoomDetail.replacingOccurrences(of: "{roomId}", with: roomID)
+        case .editRoomInfo(let roomID, _):
+            return Paths.editRoomInfo.replacingOccurrences(of: "{roomId}", with: roomID)
+        case .deleteRoom(let roomID):
+            return Paths.deleteRoom.replacingOccurrences(of: "{roomId}", with: roomID)
+            
+        case .enterRoom:
+            return Paths.enterRoom
+        case .matchRoom(let roomID):
+            return Paths.matchRoom.replacingOccurrences(of: "{roomId}", with: roomID)
+        case .exitRoom(let roomID):
+            return Paths.exitRoom.replacingOccurrences(of: "{roomId}", with: roomID)
+        case .deleteHistoryRoom(let roomID):
+            return Paths.deleteHistoryRoom.replacingOccurrences(of: "{roomId}", with: roomID)
+        }
+    }
+    
+    var method: HTTPMethod {
+        switch self {
+        case .createRoom:
+            return .post
+        case .getEnteredAllRoom:
+            return .get
+        case .getRoomInfo:
+            return .get
+        case .editRoomInfo:
+            return .patch
+        case .deleteRoom:
+            return .delete
+            
+        case .enterRoom:
+            return .post
+        case .matchRoom:
+            return .post
+        case .exitRoom:
+            return .delete
+        case .deleteHistoryRoom:
+            return .delete
+        }
+    }
+    
+    var task: Task {
+        switch self {
+        case .createRoom(let request):
+            return .requestJSONEncodable(request)
+        case .getEnteredAllRoom:
+            return .requestPlain
+        case .getRoomInfo:
+            return .requestPlain
+        case .editRoomInfo(_, let request):
+            return .requestJSONEncodable(request)
+        case .deleteRoom:
+            return .requestPlain
+            
+            
+        case .enterRoom(let request):
+            return .requestJSONEncodable(request)
+        case .matchRoom:
+            return .requestPlain
+        case .exitRoom:
+            return .requestPlain
+        case .deleteHistoryRoom:
+            return .requestPlain
+        }
+    }
+}
