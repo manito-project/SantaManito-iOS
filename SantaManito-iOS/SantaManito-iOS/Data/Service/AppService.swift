929ae296fadde7ae06d9ad895937c8f92de5ecde
Melt <seokwoo2000@naver.com>
2024-10-08 14:49:38 +0900

    [Fix] #61 - getAppStoreVersion 비동기 함수로 수정(URLSession으로 통신)

--- SantaManito-iOS/SantaManito-iOS/Data/Service/AppService.swift
+++ SantaManito-iOS/SantaManito-iOS/Data/Service/AppService.swift
@@ -7,21 +7,24 @@
 
 import Foundation
 import UIKit
+import Combine
 
 
 protocol AppServiceType {
-    func isLatestVersion() -> Bool
+    func isLatestVersion() -> AnyPublisher<Bool, Never>
     func getLocalAppVersion() -> Version
-    func getAppStoreVersion() -> Version
+    func getAppStoreVersion() -> AnyPublisher<Version, Error>
     func getDeviceIdentifier() -> String?
 }
 
 struct AppService: AppServiceType {
     
-    func isLatestVersion() -> Bool {
-        let local = getLocalAppVersion()
-        let appStore = getAppStoreVersion()
-        return local >= appStore
+    func isLatestVersion() -> AnyPublisher<Bool, Never> {
+        getAppStoreVersion()
+            .map { (appStore: $0, local: getLocalAppVersion()) }
+            .map { $0.appStore <= $0.local }
+            .catch { _ in return Just(true)} // 앱스토어 버전을 가져오지 못하면 항상 최신 버전으로 처리
+            .eraseToAnyPublisher()
     }
     
     func getLocalAppVersion() -> Version {
@@ -31,19 +34,29 @@ struct AppService: AppServiceType {
         return Version(version)
     }
     
-    func getAppStoreVersion() -> Version {
-        guard let bundleID = Bundle.main.infoDictionary?["CFBundleIdentifier"] as? String 
-        else { return Version("0.0.0") }
-        
-        let appStoreUrl = "http://itunes.apple.com/lookup?bundleId=\(bundleID)"
-        guard let url = URL(string: appStoreUrl),
-              let data = try? Data(contentsOf: url),
-              let json = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any],
-              let results = json["results"] as? [[String: Any]],
-              let appStoreVersion = results[0]["version"] as? String
-        else { return Version("0.0.0") }
-        
-        return Version(appStoreVersion)
+    func getAppStoreVersion() -> AnyPublisher<Version, Error> {
+        Just(())
+            .tryMap {
+                guard let bundleID = Bundle.main.infoDictionary?["CFBundleIdentifier"] as? String,
+                      let url = URL(string: "http://itunes.apple.com/lookup?bundleId=" + bundleID)
+                else { throw NSError() }
+                return url
+            }
+            .flatMap {
+                URLSession.shared.dataTaskPublisher(for: $0)
+                    .tryMap { output in
+                        guard let response = output.response as? HTTPURLResponse,
+                              (200...299).contains(response.statusCode)
+                        else {
+                            throw NSError()
+                        }
+                        return output.data
+                    }
+            }
+            .decode(type: [AppStoreResponse].self, decoder: JSONDecoder())
+            .compactMap { $0.first?.result.version }
+            .map { Version($0) }
+            .eraseToAnyPublisher()
     }
     
     // App Store에 설치되어 있지 않으면 bundleID를 반환한다.
@@ -54,16 +67,16 @@ struct AppService: AppServiceType {
 
 struct StubAppService: AppServiceType {
     
-    func isLatestVersion() -> Bool {
-        return true
+    func isLatestVersion() -> AnyPublisher<Bool, Never> {
+        Just(true).eraseToAnyPublisher()
     }
     
     func getLocalAppVersion() -> Version {
         return Version("1.0.0")
     }
     
-    func getAppStoreVersion() -> Version {
-        return Version("1.2.2")
+    func getAppStoreVersion() -> AnyPublisher<Version, Error> {
+        Just(Version("1.2.2")).setFailureType(to: Error.self).eraseToAnyPublisher()
     }
     
     func getDeviceIdentifier() -> String? {
