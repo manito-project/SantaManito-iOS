fad04e557386a2f9b4ae7b458bac388906042826
Melt <seokwoo2000@naver.com>
2024-10-11 13:46:18 +0900

    [Feat] #65 - accessToken 설정

--- SantaManito-iOS/SantaManito-iOS/Networks/Foundation/APIConstants.swift
+++ SantaManito-iOS/SantaManito-iOS/Networks/Foundation/APIConstants.swift
@@ -26,28 +26,14 @@ extension APIConstants{
     static var noTokenHeader: Dictionary<String,String> {
         [contentType: applicationJSON]
     }
+
+    static var hasTokenHeader: Dictionary<String,String> {
+        return [
+            contentType: applicationJSON,
+            auth : "Bearer " + UserDefaultsService.accessToken
+        ]
+    }
 }
-    
-//    static var hasTokenHeader: Dictionary<String,String> {
-//        [contentType: applicationJSON,
-////               auth : "Bearer \(UserDefaultsManager.zoocAccessToken)"]
-//    }
-//    
-//    static var multipartHeader: Dictionary<String,String> {
-//        [contentType: multipartFormData,
-//               auth : UserDefaultsManager.zoocAccessToken]
-//    }
-//    
-//    static var multipartHeaderWithBoundary: Dictionary<String,String> {
-//        [contentType: "multipart/form-data; boundary=\(APIConstants.boundary)",
-//               auth : UserDefaultsManager.zoocAccessToken]
-//    }
-//    
-//    static var refreshHeader: Dictionary<String,String> {
-//        [contentType: applicationJSON,
-//               auth : "Bearer \(UserDefaultsManager.zoocAccessToken)",
-//             refresh: "Bearer \(UserDefaultsManager.zoocRefreshToken)",
-//                 fcm: UserDefaultsManager.fcmToken]
-//    }
+
 
 
