38c6899704b5d0b79ec390992de9cc4ccb009f48
Melt <seokwoo2000@naver.com>
2024-11-30 18:16:29 +0900

    [Test] #94 - RoomEntity 나, 나의 미션 찾는 로직 단위테스트

--- SantaManito-iOS/SantaManitoUnitTests/RoomEntityTests/RoomDetailEntityTests.swift
+++ SantaManito-iOS/SantaManitoUnitTests/RoomEntityTests/RoomDetailEntityTests.swift
@@ -9,11 +9,11 @@ import XCTest
 @testable import SantaManito_iOS
 
 final class RoomDetailEntityTests: XCTestCase {
-
+    
     override func setUp()  {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
     }
-
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
@@ -29,7 +29,70 @@ final class RoomDetailEntityTests: XCTestCase {
         XCTAssertEqual(states2.sorted(), expected)
     }
     
+    func test_나를_잘_찾는가() {
+        
+        // Given
+        UserDefaultsService.shared.userID = "test_user_id_1"
+        
+        let santa1 = SantaUser(id: "test_user_id_1", username: "나", missionId: nil)
+        let manitto1 = User(id: "test_user_id_2", username: "너")
+        
+        let santa2 = SantaUser(id: "test_user_id_2", username: "너", missionId: nil)
+        let manitto2 = User(id: "test_user_id_1", username: "나")
+        
+        let member1 = Member(santa: santa1, manitto: manitto1) // 나
+        let member2 = Member(santa: santa2, manitto: manitto2) // 너
+        
+        // When
+        let roomDetail = RoomDetail(
+            id: "",
+            name: "",
+            invitationCode: "",
+            state: .completed,
+            creatorID: "test_user_id_1",
+            creatorName: "",
+            members: [member1, member2],
+            mission: [],
+            createdAt: Date(),
+            expirationDate: Date())
+        
+        // Then
+        XCTAssertEqual(roomDetail.me, member1) // 
+    }
     
-
-
+    func test_나의_미션을_잘_찾는가() {
+        
+        // Given
+        UserDefaultsService.shared.userID = "test_user_id_1"
+        
+        let santa1 = SantaUser(id: "test_user_id_1", username: "나", missionId: "mission_id_1")
+        let manitto1 = User(id: "test_user_id_2", username: "너")
+        let mission1 = Mission(content: "나의 미션", id: "mission_id_1")
+        
+        let santa2 = SantaUser(id: "test_user_id_2", username: "너", missionId: "mission_id_2")
+        let manitto2 = User(id: "test_user_id_1", username: "나")
+        let mission2 = Mission(content: "너의 미션", id: "mission_id_2")
+        
+        let member1 = Member(santa: santa1, manitto: manitto1) // 나
+        let member2 = Member(santa: santa2, manitto: manitto2) // 너
+        
+        // When
+        let roomDetail = RoomDetail(
+            id: "",
+            name: "",
+            invitationCode: "",
+            state: .completed,
+            creatorID: "test_user_id_1",
+            creatorName: "",
+            members: [member1, member2],
+            mission: [mission1, mission2],
+            createdAt: Date(),
+            expirationDate: Date())
+        
+        // Then
+        XCTAssertEqual(roomDetail.myMission, mission1)
+    }
+    
+    
+    
 }
