38f2d13971719773d94b59ee5e03de5e0f93bb8b
Melt <seokwoo2000@naver.com>
2025-03-07 01:21:32 +0900

    [Feat] #112 - 방 텍소노미 적용

---
+++ SantaManito-iOS/SantaManito-iOS/Feature/MakeRoom/MakeRoomTaxonomy.swift
@@ -0,0 +1,23 @@
+//
+//  MakeRoomTaxonomy.swift
+//  SantaManito-iOS
+//
+//  Created by 장석우 on 3/7/25.
+//
+
+import Foundation
+
+extension AnalyticsTaxonomy {
+    
+    static let roomEdit = AnalyticsTaxonomy(
+        tag: "마니또방_방정보수정",
+        tagEng: "room_edit",
+        type: .page
+    )
+    
+    static let roomEditCompleteBtn = AnalyticsTaxonomy(
+        tag: "마니또방_방정보수정_수정완료버튼",
+        tagEng: "room_edit_complete_btn",
+        type: .button
+    )
+}
